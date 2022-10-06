d8668ee9019adf0b5dd0818aee2115e82aa61232
Gustavo M. Costa <101591580+gustavo1902@users.noreply.github.com>
2022-10-06 12:19:55 -0300

    Update prog.c

--- prog/prog.c
+++ prog/prog.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main ()
+{
+    int x = 0;
+    //x = x + 1;
+    x += 1;
+
+    while (x < 10)
+    {
+        printf("msg\n");
+        x += 1;
+    }
+    return 0;
+}
