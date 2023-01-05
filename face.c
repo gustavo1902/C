57565344311476c5e5906e13c8dbd3efdcf4313b
Gustavo M. Costa <101591580+gustavo1902@users.noreply.github.com>
2023-01-05 20:51:21 -0300

    terceira versão

---
+++ face.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  // Draw the head
+  printf("  _______   \n");
+ // printf(" /     \\ \n");
+ // printf("|       |\n");
+  printf("  \\_____/ \n");
+
+  // Draw the eyes
+  printf(" ( o   o )\n");
+
+  // Draw the nose
+  printf("  \\  ^  / \n");
+
+  // Draw the mouth
+  printf("   \\___/  \n");
+
+  return 0;
+}
